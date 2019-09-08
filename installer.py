9f51fe10accf6dd044c7c0e4d8b9033cffd21e9e
Praman Kasliwal (C|EH) <kasliwal.praman008@gmail.com>
2019-09-08 06:29:15 +0530

    Updated...

--- installer.py
+++ installer.py
@@ -5,10 +5,10 @@ import sys
 
 def main():
 	if not os.geteuid() == 0:
-    	sys.exit("""\033[1;91m\n[!] Knocker installer must be run as root. ¯\_(ツ)_/¯\n\033[1;m""")
-    else:
-    	print ("Welcome to Knocker! Please be patient while knocker installs... It will hardly take a moment to install...", end="\n")
-    	install = os.system("""sudo chmod +x knocker.py && sudo mkdir /usr/bin/knocker && sudo cp knocker.py /usr/bin/knocker/knocker && tput setaf 34; echo "Knocker has been sucessfuly installed. Execute 'knocker -h' to understand the working." """)
+		sys.exit("""\033[1;91m\n[!] Knocker installer must be run as root. ¯\_(ツ)_/¯\n\033[1;m""")
+	else:
+		print ("Welcome to Knocker! Please be patient while knocker installs... It will hardly take a moment to install...", end="\n")
+		install = os.system("""sudo chmod +x knocker.py && sudo mkdir /usr/bin/knocker && sudo cp knocker.py /usr/bin/knocker/knocker && tput setaf 34; echo "Knocker has been sucessfuly installed. Execute 'knocker -h' to understand the working." """)
 
 if __name__ == "__main__":
 	main()
